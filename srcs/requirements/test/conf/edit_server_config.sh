adbf9b2c4d5cde97c2c87f1861e218759dc43a91
lorbke <lorbke@student.42heilbronn.de>
2023-12-04 17:14:24 +0100

    added nginx container

--- srcs/requirements/test/conf/edit_server_config.sh
+++
@@ -1,8 +0,0 @@
-#!bin/sh
-
-{ echo '[mysqld]'; \
-	echo 'skip-host-cache'; \
-	echo 'skip-name-resolve'; \
-	echo 'bind-address=0.0.0.0'; \
-} | tee  /etc/my.cnf.d/docker.cnf; \
-sed -i "s|skip-networking|skip-networking=0|g" /etc/my.cnf.d/mariadb-server.cnf
