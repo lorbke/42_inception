f1c0e9467bc552f4e205e676e5f344beeb912e88
lorbke <lorbke@student.42heilbronn.de>
2023-12-06 18:13:36 +0100

    fixed styling issues

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-sleep 2
+sleep 3
 
 while ! nc -z mariadb 3306; do
   sleep 1
 done
 
-if ! wp core is-installed --allow-root --path=/var/www/html/wordpress; then
-  cd /var/www/html/wordpress
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+  wp core download --path=/var/www/html/ --allow-root
+  cd /var/www/html/
   wp config create --dbname=$DB_NAME --dbhost=$DB_HOST \
   --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
-  wp core install --url="localhost" --title="WordPress Site" \
+  wp core install --skip-email --url=$DOMAIN_NAME --title="WordPress Site" \
   --admin_user="master" --admin_password="master" \
-  --admin_email="master@example.com" --path=/var/www/html/wordpress --allow-root
+  --admin_email="master@example.com" --path=/var/www/html/ --allow-root
   wp user create "author" "author@example.com" --role=author --user_pass=$DB_PASSWORD \
-  --path=/var/www/html/wordpress --allow-root
+  --path=/var/www/html/ --allow-root
 fi
 
 cd /
