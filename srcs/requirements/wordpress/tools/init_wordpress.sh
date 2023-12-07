ab285f0890397b916cb19934fdc118a01e6dd37b
lorbke <luca.orbke@brinkjost.de>
2023-12-07 19:26:37 +0100

    added wp environment variables

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -12,9 +12,9 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
   wp config create --dbname=$DB_NAME --dbhost=$DB_HOST \
   --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
   wp core install --skip-email --url=$DOMAIN_NAME --title="WordPress Site" \
-  --admin_user="master" --admin_password="master" \
-  --admin_email="master@example.com" --path=/var/www/html/ --allow-root
-  wp user create "author" "author@example.com" --role=author --user_pass="author" \
+  --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD \
+  --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html/ --allow-root
+  wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD \
   --path=/var/www/html/ --allow-root
 fi
 
